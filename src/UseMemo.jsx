ca1a77cde26532eb56ffa9da97fff0375e0f6983
Pitambarbittu <99995315+Pitambarbittu@users.noreply.github.com>
2022-09-07 19:51:57 +0530

    Update UseMemo.jsx

--- src/UseMemo.jsx
+++ src/UseMemo.jsx
@@ -17,7 +17,7 @@ export default function UseMemo(){
     return(
         <>
          <h1>5- What is UseMemo Hook ?(Implementation)</h1>
-         <p>The React useMemo Hook returns a memoized value.Think of memoization as caching a value so that it does not need to be recalculated.The useMemo Hook only runs when one of its dependencies update.This can improve performance. The useMemo and useCallback Hooks are similar. The main difference is that useMemo returns a memoized value and useCallback returns a memoized function. You can learn more about useCallback in the useCallback chapter.</p>
+         <p>The React useMemo Hook returns a memoized value.Think of memoization as caching a value so that it does not need to be recalculated.The useMemo Hook only runs when one of its dependencies update.This can improve performance. The useMemo and useCallback Hooks are similar. The main difference is that useMemo returns a memoized value and useCallback returns a memoized function.</p>
 
          <h2>Value: {val}</h2>
          <h2>Items: {item} </h2>
