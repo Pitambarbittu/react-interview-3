39401a0eba113c646ce86b1049bb2be4d37a6ea0
Pitambarbittu <99995315+Pitambarbittu@users.noreply.github.com>
2022-09-07 16:55:16 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Parent from './Usestate/Parent';
+import Usereducer from './Usereducer';
+import UseMemo from './UseMemo'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      <Parent/>
+      <Usereducer/>
+      <UseMemo/>
     </div>
   );
 }
